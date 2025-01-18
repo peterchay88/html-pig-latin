9547676810d5986b59abcd7095e750699a83fd03
Peter Chay <peterchay88@gmail.com>
2025-01-17 22:21:23 -0700

    initial commit

---
+++ src/javaScript/main.js
@@ -0,0 +1,18 @@
+// Define html elements
+const translateBox = document.getElementById("text-box");
+const translateButton = document.getElementById("translate-button");
+
+function translateFunction(){ 
+    const text = translateBox.value;
+
+    alert(text);
+}
+
+// function test(){
+//     const myButton = document.getElementById("my-btn");
+    
+
+//     myButton.style.width = "500px";
+//     myButton.style.backgroundColor = "blue";
+//     document.body.style.backgroundColor = "red";
+// }
