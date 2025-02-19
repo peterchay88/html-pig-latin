9915d82562ed237a13dfba2f6808891fa40df9af
Peter Chay <peterchay88@gmail.com>
2025-02-18 22:26:39 -0700

    Messing with the styles

--- src/javaScript/settings.js
+++ src/javaScript/settings.js
@@ -61,19 +61,71 @@ class Settings {
         this.header.style.color = "yellow";
         this.textContainer[0].style.border = "white";
         this.textContainer[0].style.borderStyle = "solid";
-        this.applyButton.style.backgroundColor = "purple";
+        this.applyButton.style.backgroundColor = "green";
+        this.backButton.style.color = "yellow";
+        this.backButton.style.backgroundColor = "black";
+        this.backButton.style.border = "white";
+        this.backButton.style.borderStyle = "solid";
+        this.themeDropdown.style.backgroundColor = "black";
+        this.themeDropdown.style.color = "yellow";
+
+        // Event listen for hover over back button
+        this.addHoverEvents();
+       
     }
+
+    /**
+     * High contrast mouse over event
+     */
+    highContrastHover() {
+            this.backButton.style.backgroundColor = "yellow";
+            this.backButton.style.color = "black"; 
+        }
+    
+    /**
+     * High contrast mouse out event
+     */
+    highContrastOut() {
+            this.backButton.style.color = "yellow";
+            this.backButton.style.backgroundColor = "black";
+            this.backButton.style.border = "white";
+            this.backButton.style.borderStyle = "solid";
+        }
+    
+    /**
+     * Add hover events for high contrast theme
+     */
+    addHoverEvents(){
+        this.backButton.addEventListener("mouseover", () => this.highContrastHover());
+        this.backButton.addEventListener("mouseout", () => this.highContrastOut());
+    }
+
+    /**
+     * Remove hover events for high contrast theme
+     */
+    removeHoverEvents(){
+        this.backButton.removeEventListener("mouseover", this.highContrastHover);
+        this.backButton.removeEventListener("mouseout", this.highContrastOut);
+        console.log("Hover events removed")
+    }
+    
 
     /**
      * Reset inline styles to default
      */
     resetStyles() {
-        var pageElements = [this.applyButton, this.textContainer[0], this.header, 
-                            this.backButton, this.themeDropdown];
+        // Array of elements to reset
+        var pageElements = [this.applyButton, this.textContainer[0], 
+            this.header, this.backButton, this.themeDropdown];
+            
+        // Loop through array and reset styles
         for  (let i = 0; i < pageElements.length; i++) {
             pageElements[i].removeAttribute("style");
             console.log(`${pageElements[i]} reset to default`)
         }
+
+        // Remove hover events
+        this.removeHoverEvents();
 
     }
 }
