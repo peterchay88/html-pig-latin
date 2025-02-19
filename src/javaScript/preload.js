e44117676e8f8dcad14832b5c56601d4b5dcaa49
Peter Chay <peterchay88@gmail.com>
2025-02-18 22:45:18 -0700

    clearing cache for git ignore file

--- src/javaScript/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('versions', {
-    // Pull version numbers 
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron
-  })
